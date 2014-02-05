2391ece9d17854e4a84b8992a42b4ded20eb54e9
Denys <Denys@Raumrepola>
2014-02-05 08:57:50 +0200

    Cross class was created

---
+++ CrossesGameWithGit/src/Cross.java
@@ -0,0 +1,26 @@
+
+public class Cross {
+	private int xPosition;
+	private int yPosition;
+	
+	public void setXPosition(int x){
+		if (x>=0 && x <=3){
+			xPosition = x;
+		}else{
+			System.out.println("Invalid x position");
+		}
+		
+	}
+	
+	public void setYPosition(int y){
+		if (y>=0 && y <=3){
+			yPosition = y;
+		}else{
+			System.out.println("Invalid y position");
+		}
+		
+	}
+	
+	
+	
+}
